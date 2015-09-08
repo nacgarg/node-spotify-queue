3ab25aaa8a6f86362d67aede438256ecdf553950
Nachi G <nacgarg@users.noreply.github.com>
2015-09-08 10:05:38 -0700

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,6 +1,6 @@
 var binaryjs = require('binaryjs');
 var speaker = new require('speaker')();
-var client = new binaryjs.BinaryClient('ws://'+process.argv[2]+':9000');
+var client = new binaryjs.BinaryClient('ws://'+ (process.argv[2] || "emraldia.com")+':9000');
 
 client.on('stream', function(stream, meta) {
 	stream.pipe(speaker)
